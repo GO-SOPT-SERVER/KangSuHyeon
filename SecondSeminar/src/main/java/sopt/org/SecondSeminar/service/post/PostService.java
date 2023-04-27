a3eb011479cb62b7be2a118e8866dfc49634a56a
onpyeong <gsh447114@gmail.com>
2023-04-27 12:01:52 +0900

    2주차 심화과제

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/service/post/PostService.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/service/post/PostService.java
@@ -2,6 +2,7 @@ package sopt.org.SecondSeminar.service.post;
 
 import org.springframework.stereotype.Service;
 import sopt.org.SecondSeminar.controller.post.dto.CreatePostDTO;
+import sopt.org.SecondSeminar.controller.post.dto.UpdatePostDTO;
 import sopt.org.SecondSeminar.domain.post.Post;
 
 import java.util.Optional;
@@ -11,7 +12,7 @@ import static sopt.org.SecondSeminar.SecondSeminarApplication.postList;
 @Service
 public class PostService {
 
-    public Long createPost(CreatePostDTO post) {
+    public Long createPost(final CreatePostDTO post) {
         Post newPost = new Post(
                 post.getWriter(),
                 post.getTitle(),
@@ -19,21 +20,21 @@ public class PostService {
                 post.getLike()
         );
 
-        postList.add(newPost);
         newPost.setId((long) postList.size());
+        postList.add(newPost);
 
         return newPost.getId();
     }
 
-    public Optional<Post> getPostById(Integer postId) { //코틀린 nullable ? 대신 자바는 Optional
-        if (postList.size() >= postId) {
+    public Optional<Post> getPostById(final Integer postId) { //코틀린 nullable ? 대신 자바는 Optional
+        if (postList.size() > postId) {
             return Optional.of(postList.get(postId)); //코틀린 !! -> 무조건 null이 아님
         } else {
             return Optional.empty(); //null 객체
         }
     }
 
-    public String getPostByTitle(String title) {
+    public String getPostByTitle(final String title) {
         StringBuilder resultPostList = new StringBuilder();
         postList.forEach((post) -> {
                     if (title.equals(post.getTitle())) {
@@ -44,5 +45,28 @@ public class PostService {
         );
 
         return resultPostList.toString();
+    }
+
+    public Optional<String> updatePost(final int postId, final UpdatePostDTO post) {
+        if (postId < postList.size()) {
+            Post updatePost = new Post(
+                    post.getWriter(),
+                    post.getTitle(),
+                    post.getContent(),
+                    postList.get(postId).getLike()
+            );
+            updatePost.setId((long) postId);
+            postList.set(postId, updatePost);
+            return Optional.of(postList.get(postId).toString());
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Integer> deletePost(final int postId) {
+        if (postId < postList.size()) {
+            postList.remove(postId);
+            return Optional.of(postId);
+        }
+        return Optional.empty();
     }
 }
